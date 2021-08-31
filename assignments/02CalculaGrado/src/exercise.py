a5b048402e9bbe843ba11ff937d460b5ad0a47c3
BryantOchoa <A01252419@itesm.mx>
2021-08-31 03:36:45 +0000

    tarea 2 bryant

--- assignments/02CalculaGrado/src/exercise.py
+++ assignments/02CalculaGrado/src/exercise.py
@@ -1,7 +1,17 @@
+def calcula_grado(x):
+    if 0.90 <= x < 0.99:
+        return 'A'
+    elif 0.80 <= x < 0.90:
+        return 'C'
+    elif 0.70 <= x < 0.80:
+        return 'B'
+    elif 0.60 <= x < 0.70:
+        return 'D'
+    else:
+        return "score incorrecto"
 def main():
-    #escribe tu código abajo de esta línea
-    x = float(input("Ingresa Un valor entre 0.0 y 1.0: "))
+    x = float(input('Ingresa Un valor entre 0.0 y 1.0: '))
     print(calcula_grado(x))
-
 if __name__=='__main__':
     main()
+
